5016d9096cf07380c18522be07db6e3a54a612cb
Antonio <antoniovictorbordignon@outlook.com>
2022-12-05 13:38:19 -0300

    responsiveness adjustment

--- cash-register.js
+++ cash-register.js
@@ -1,3 +1,6 @@
+
+
+
 function checkCashRegister(price, cash, cid) {
   let change = cash*100 - price*100;
   let cidTotal = 0;
@@ -31,4 +34,4 @@ function checkCashRegister(price, cash, cid) {
   }
 }
 
-console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+//checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
