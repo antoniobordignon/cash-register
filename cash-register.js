e97cafe4881e595fceb5cb691398e60244b21d76
Antonio <antoniovictorbordignon@outlook.com>
2022-12-05 17:44:33 -0300

    showing answer

--- cash-register.js
+++ cash-register.js
@@ -1,43 +1,33 @@
+function checkCashRegister() {
+  let penny = Number(document.getElementById('penny').value)
+  let nickel = Number(document.getElementById('nickel').value)
+  let dime = Number(document.getElementById('dime').value)
+  let quarter = Number(document.getElementById('quarter').value)
+  let one = Number(document.getElementById('one').value)
+  let five = Number(document.getElementById('five').value)
+  let ten = Number(document.getElementById('ten').value)
+  let twenty = Number(document.getElementById('twenty').value)
+  let oneHundred = Number(document.getElementById('oneHundred').value)
+  let productsPrice = Number(document.getElementById('productsPrice').value)
+  let cashReceived = Number(document.getElementById('cashReceived').value)
 
+  let answerHtml = document.getElementById('answer')
 
+  let cid = [['PENNY', penny], [' NICKEL', nickel], [' DIME', dime], [' QUARTER', quarter], [' ONE', one], [' FIVE', five], [' TEN', ten], [' TWENTY', twenty], [' ONE HUNDRED', oneHundred]]; 
 
-function checkCashRegister(price, cash, cid) {
-  let pennyValue = document.getElementById('penny')
-  let penny = Number(pennyValue.value)
-  let nickelValue = document.getElementById('nickel')
-  let nickel = Number(nickelValue.value)
-  let dimeValue = document.getElementById('dime')
-  let dime = Number(dimeValue.value)
-  let quarterValue = document.getElementById('quarter')
-  let quarter = Number(quarterValue.value)
-  let oneValue = document.getElementById('one')
-  let one = Number(oneValue.value)
-  let fiveValue = document.getElementById('five')
-  let five = Number(fiveValue.value)
-  let tenValue = document.getElementById('ten')
-  let ten = Number(tenValue.value)
-  let twentyValue = document.getElementById('twenty')
-  let twenty = Number(twentyValue.value)
-  let oneHundredValue = document.getElementById('oneHundred')
-  let oneHundred = Number(oneHundredValue.value)
-  let productsValue = document.getElementById('products')
-  let products = Number(productsValue.value)
-  let cashReceivedValue = document.getElementById('cashReceived')
-  let cashReceived = Number(cashReceivedValue.value)
-
-  let change = cash*100 - price*100;
+  let change = cashReceived*100 - productsPrice*100;
   let cidTotal = 0;
   for (let elem of cid){
     cidTotal += elem[1]*100
   } 
   if (change > cidTotal){
-    return {status: "INSUFFICIENT_FUNDS", change: []}
+    return answerHtml.innerHTML = 'status: "INSUFFICIENT_FUNDS", change: 0'
   } else if (change === cidTotal) {
-    return {status: "CLOSED", change: cid}
+    return answerHtml.innerHTML = `status: "CLOSED", change: ${cid}`
   } else {
     let answer = []
     cid=cid.reverse()
-    let moneyUnits = {'ONE HUNDRED': 10000, 'TWENTY': 2000, 'TEN': 1000, 'FIVE': 500,'ONE': 100, 'QUARTER': 25, 'DIME': 10, 'NICKEL': 5, 'PENNY': 1}
+    let moneyUnits = {' ONE HUNDRED': 10000, ' TWENTY': 2000, ' TEN': 1000, ' FIVE': 500,' ONE': 100, ' QUARTER': 25, ' DIME': 10, ' NICKEL': 5, 'PENNY': 1}
     for (let elem of cid){
       let holder = [elem[0], 0]
       elem[1]=elem[1]*100
@@ -51,10 +41,9 @@ function checkCashRegister(price, cash, cid) {
       }
     }
     if (change > 0){
-    return {status: "INSUFFICIENT_FUNDS", change: []}
+      return answerHtml.innerHTML =  'status: "INSUFFICIENT_FUNDS", change: 0'
     }
-    return {status: "OPEN", change: answer}
+      return answerHtml.innerHTML = `status: "OPEN", change: ${answer}`
+
   }
 }
-
-//checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
